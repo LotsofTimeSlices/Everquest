99a19e48dd37775f82b460330a40e095b651471f
Roger S <35031699+LotsofTimeSlices@users.noreply.github.com>
2019-08-18 10:29:55 -0500

    Set-ConstantAffinity
    Opens PowerShell window that continually checks for renamed EQ process that is not set for processor affinity.

---
+++ Set-ConstantAffinity.ps1
@@ -0,0 +1,20 @@
+﻿$host.ui.RawUI.WindowTitle = 'Set-ConstantAffinity'
+$procAffinity = [Int32]([math]::pow(2, $(Get-WmiObject -class Win32_processor).NumberOfLogicalProcessors) - 1) #processor affinity number based on # of logical cores
+Do
+{
+	Do
+	{
+		Start-Sleep -Seconds 4
+	}
+	While ((Get-Process eqgame).MainWindowTitle -match 'Everquest') #does nothing until all EQ windows have been renamed by MQ2AutoLogin    
+	Write-Host 'Checking Processor Affinity...'
+	$AffinityNotSet = Get-Process eqgame | Where-Object { $_.ProcessorAffinity -ne $procAffinity }
+	ForEach ($A in $AffinityNotSet) #Set processes to affinity who are not set
+	{
+		Write-Host "Setting toon to affinity:  $($A.MainWindowTitle)"
+		$A.ProcessorAffinity = $procAffinity
+	}
+	Write-Host 'Deep Sleep...'
+	Start-Sleep -Seconds 25 #Deep Sleep
+}
+while (Get-Process eqgame) #Runs until EQ is closed
